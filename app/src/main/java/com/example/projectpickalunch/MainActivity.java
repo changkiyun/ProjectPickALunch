8c424679990a146604f56c73be2f543e3932c660
changkiyun <53373279+changkiyun@users.noreply.github.com>
2022-06-02 19:04:22 +0900

    메인화면

--- app/src/main/java/com/example/projectpickalunch/MainActivity.java
+++ app/src/main/java/com/example/projectpickalunch/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.projectpickalunch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+    private GridView m_grid;
+    private MainGridAdapter m_gridAdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        m_grid = (GridView)findViewById(R.id.grid_test);
+        m_gridAdt = new MainGridAdapter(getApplicationContext() );
+
+        for (int i = 0 ; i < 100 ; i++ ) {
+            String strNo = "Num : " + i;
+            m_gridAdt.setItem(strNo);
+        }
     }
 }
