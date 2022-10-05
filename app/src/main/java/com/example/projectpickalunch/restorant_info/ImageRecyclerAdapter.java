31f937534d870a2f415d153abe602ed1fed218c1
UngHa <104884079+UngHa@users.noreply.github.com>
2022-10-05 18:45:41 +0900

    식당 정보 세부 사진 리사이클러뷰, 테스트용 임시 이미지 추가 버튼 생성(추후 삭제 또는 재사용)

---
+++ app/src/main/java/com/example/projectpickalunch/restorant_info/ImageRecyclerAdapter.java
@@ -0,0 +1,56 @@
+package com.example.projectpickalunch.restorant_info;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.projectpickalunch.R;
+
+import java.util.ArrayList;
+
+
+public class ImageRecyclerAdapter extends RecyclerView.Adapter<ImageRecyclerAdapter.TestViewHolder> {
+
+    Context context;
+
+    ArrayList<RecyclerImageItem> testlist = new ArrayList<>();
+
+    public ImageRecyclerAdapter(Context context, ArrayList<RecyclerImageItem> testlist) {
+        this.context = context;
+        this.testlist = testlist;
+    }
+
+    @NonNull
+    @Override
+    public TestViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.recycler_item, parent, false);
+
+        return new TestViewHolder(view);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull TestViewHolder holder, int position) {
+        Glide.with(context).load(testlist.get(position).getImageUrl()).into(holder.mImageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return testlist.size();
+    }
+
+    class TestViewHolder extends RecyclerView.ViewHolder{
+        ImageView mImageView;
+
+        public TestViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mImageView = itemView.findViewById(R.id.item_image);
+        }
+    }
+}
